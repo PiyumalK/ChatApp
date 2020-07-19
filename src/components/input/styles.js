fc4bad7339541b35e1ecf48875381ee965e03f40
Piyumal Karunathilaka <piyuk.thilaka@gmail.com>
2020-07-19 15:05:20 +0530

    Implemented Login page

---
+++ src/components/input/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { appStyle } from '../../utility/styleHelper/appStyle';
+
+export default StyleSheet.create({
+    input: {
+        paddingLeft: 16,
+        bachgroundColor: appStyle,fieldBgColor,
+        width: "90%",
+        color: appStyle.fieldTextColor,
+        height: appStyle.fieldHeight,
+        alignSelf: "center",
+        marginVertical: appStyle.fieldMarginVertical,
+        fontSize: 16,
+    },
+});
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,11 +1,64 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { globalStyle, color } from '../../utility';
+import Logo from '../../components/logo';
+import Inputfield from '../../components/inputfield';
+import RoundCornerButton from '../../components/buttons';
 
 const Login = ({navigation}) => {
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: '',
+    });
+    const {email, password} = credentials;
+
+    onLoginPress = () => {
+        if(!email){
+            alert('Email is required!');
+        } else if(!password){
+            alert('Password is required!');
+        } else {
+            alert(JSON.stringify(credentials));
+        }
+    };
+
+    const handleOnChange = (name, value) => {
+        setCredentials({
+            ...credentials,
+            [name]: value,
+        });
+    };
+
     return (
-        <SafeAreaView>
-            <Text onPress={() => navigation.navigate('SignUp')}>Login</Text>
+        <SafeAreaView style={[ globalStyle.flex1, {backgroundColor: color.LIGHT_STEEL_BLUE}]}>
+            <View style={[globalStyle.containerCentered]}>
+                <Logo />
+            </View>
+            <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
+                <Inputfield
+                    placeholder="Enter email"
+                    value={email}
+                    onChangeText={(text) => handleOnChange('email', text)}
+                />
+                <Inputfield
+                    placeholder="Enter password"
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={(text) => handleOnChange('password', text)}
+                />
+                <RoundCornerButton title="Login " onPress={() => onLoginPress()} />
+                <Text
+                    style={{
+                        fontSize: 28,
+                        fontWeight: 'bold',
+                        color: color.BLACK,
+                        textAlign: 'center',
+                    }}
+                    onPress={() => navigation.navigate('SignUp')}>
+                        Sign Up
+                </Text>
+            </View>
         </SafeAreaView>
     );
 };
